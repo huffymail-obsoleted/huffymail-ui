e8242b7de54490122f6ce437ce022719d8476d95
Irving Dinh <irving.dinh@gmail.com>
2023-02-04 14:11:44 +0700

    Integrated with Tailwind UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
+    './src/**/*.{js,jsx,ts,tsx}'
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
   plugins: [],
 }
